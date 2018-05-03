1071c50323fd31eb126b31c7e4f0f06ee257e3ae
dedoxys <pepevelezcuadrado@gmail.com>
2018-05-03 16:02:31 +0200

    first commit

---
+++ src/ejercicioPatronesCreacionalesjmvc/Persona.java
@@ -0,0 +1,43 @@
+package ejercicioPatronesCreacionalesjmvc;
+
+public class Persona {
+	
+	private String colorOjos;
+	private String colorPelo;
+	private String tipoPelo;
+	private String tipoOjos;
+	private String nombre;
+	private int edad;
+	private String ciudad;
+	private int peso;
+	private float altura;
+	
+	public String getColorOjos() {
+		return this.colorOjos;
+	}
+	public String getColorPelo() {
+		return this.colorPelo;
+	}
+	public String getTipoPelo() {
+		return this.tipoPelo;
+	}
+	public String getTipoOjos() {
+		return this.tipoOjos;
+	}
+	public String getNombre() {
+		return this.nombre;
+	}
+	public int getEdad() {
+		return this.edad;
+	}
+	public String getCiudad() {
+		return this.ciudad;
+	}
+	public int getPeso() {
+		return this.peso;
+	}
+	public float getAltura() {
+		return this.altura;
+	}
+	
+}
