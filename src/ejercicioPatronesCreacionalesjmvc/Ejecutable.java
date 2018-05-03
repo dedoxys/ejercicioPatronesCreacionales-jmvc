e7bc317fa4b3133bfa2521449cf9d8fd4334a06a
dedoxys <pepevelezcuadrado@gmail.com>
2018-05-03 18:04:33 +0200

    corregido

--- src/ejercicioPatronesCreacionalesjmvc/Ejecutable.java
+++ src/ejercicioPatronesCreacionalesjmvc/Ejecutable.java
@@ -1,10 +1,50 @@
 package ejercicioPatronesCreacionalesjmvc;
 
-public class Ejecutable {
+import java.io.IOException;
 
+public class Ejecutable {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PersonaBuilder pb = new PersonaBuilder();
+		Persona p;
+		String linea = "";
+		try {
+			linea = Persona.leerFicheroTxt();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String []separado = linea.split("#");
+		
+		
+		for (int i = 0; i < separado.length; i++) {
+			String []propiedad = separado[i].split(":");
+			if(propiedad[0].equals("colorOjos")) {
+				pb.setColorOjos(propiedad[1]);
+			}else if(propiedad[0].equals("colorPelo")) {
+				pb.setColorPelo(propiedad[1]);
+			}else if(propiedad[0].equals("tipoPelo")) {
+				pb.setTipoPelo(propiedad[1]);
+			}else if(propiedad[0].equals("tipoOjos")) {
+				pb.setTipoOjos(propiedad[1]);
+			}else if(propiedad[0].equals("nombre")) {
+				pb.setNombre(propiedad[1]);
+			}else if(propiedad[0].equals("edad")) {
+				pb.setEdad(Integer.valueOf(propiedad[1]));
+			}else  if(propiedad[0].equals("ciudad")) {
+				pb.setCiudad(propiedad[1]);
+			}else  if(propiedad[0].equals("peso")) {
+				pb.setPeso(Integer.valueOf(propiedad[1]));
+			}else  if(propiedad[0].equals("altura")) {
+				pb.setAltura(Integer.valueOf(propiedad[1]));
+			}
+			
+			
+			
+		}
+		p = pb.construir();
+		System.out.println(p);
 	}
 
 }
--- src/ejercicioPatronesCreacionalesjmvc/Persona.java
+++ src/ejercicioPatronesCreacionalesjmvc/Persona.java
@@ -1,7 +1,12 @@
 package ejercicioPatronesCreacionalesjmvc;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Persona {
-	
+
 	private String colorOjos;
 	private String colorPelo;
 	private String tipoPelo;
@@ -10,34 +15,80 @@ public class Persona {
 	private int edad;
 	private String ciudad;
 	private int peso;
-	private float altura;
-	
+	private int altura;
+
+	public Persona(PersonaBuilder personaBuilder) {
+		this.colorOjos = personaBuilder.getColorOjos();
+		this.colorPelo = personaBuilder.getColorPelo();
+		this.tipoPelo = personaBuilder.getTipoPelo();
+		this.tipoOjos = personaBuilder.getTipoOjos();
+		this.nombre = personaBuilder.getNombre();
+		this.edad = personaBuilder.getEdad();
+		this.ciudad = personaBuilder.getCiudad();
+		this.peso = personaBuilder.getPeso();
+		this.altura = personaBuilder.getAltura();
+	}
+
 	public String getColorOjos() {
 		return this.colorOjos;
 	}
+
 	public String getColorPelo() {
 		return this.colorPelo;
 	}
+
 	public String getTipoPelo() {
 		return this.tipoPelo;
 	}
+
 	public String getTipoOjos() {
 		return this.tipoOjos;
 	}
+
 	public String getNombre() {
 		return this.nombre;
 	}
+
 	public int getEdad() {
 		return this.edad;
 	}
+
 	public String getCiudad() {
 		return this.ciudad;
 	}
+
 	public int getPeso() {
 		return this.peso;
 	}
-	public float getAltura() {
+
+	public int getAltura() {
 		return this.altura;
 	}
-	
+
+	public static String leerFicheroTxt() throws IOException {
+		File file = new File("archivo.txt");
+		String linea = "";
+		String cadena = "";
+		FileReader fr = new FileReader(file.getName());
+		BufferedReader br = new BufferedReader(fr);
+		while ((cadena = br.readLine()) != null) {
+			linea += cadena;
+			linea += "#";
+		}
+
+		br.close();
+		fr.close();
+		return linea;
+	}
+
+	public String toString() {
+		String cadena = "";
+
+		cadena += "colorOjos: " + this.colorOjos + " colorPelo: " + this.colorPelo + " tipoPelo: " + this.tipoPelo
+				+ " tipoOjos: " + this.tipoOjos + " nombre: " + this.nombre + " edad: " + this.edad + " ciudad: "
+				+ this.ciudad + " peso: " + this.peso + " altura: " + this.altura;
+
+		return cadena;
+	}
+
 }
